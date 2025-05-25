647e6785404dac59dd71cf2e709b8cfd88f3ab57
jlauza <lauzajayson@gmail.com>
2025-05-25 19:12:03 +0800

    change headings

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -4,8 +4,15 @@ export default function Page() {
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello Web!</h1>
-        <Button size="sm">Button</Button>
+        <h1 className="text-2xl font-bold">Welcome to Pro Dance X</h1>
+        <div className="flex gap-4">
+          <Button size="sm" className="cursor-pointer">
+            Admin
+          </Button>
+          <Button size="sm" variant="secondary" className="cursor-pointer">
+            Students
+          </Button>
+        </div>
       </div>
     </div>
   );
