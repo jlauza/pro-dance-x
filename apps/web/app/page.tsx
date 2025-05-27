de7d15ab6338055ae3e87afa68e8128e049c602f
jlauza <lauzajayson@gmail.com>
2025-05-27 14:17:23 +0800

    remove erros

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,26 +1,24 @@
 import { Button } from "@workspace/ui/components/button";
 import Link from "next/link";
 
+const app_admin = process.env.NEXT_PUBLIC_APP_ONE_URL;
+const app_student = process.env.NEXT_PUBLIC_APP_TWO_URL;
+
+console.log("Admin URL:", app_admin);
+console.log("Student URL:", app_student);
+
 export default function Page() {
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
         <h1 className="text-2xl font-bold">Welcome to Pro Dance X</h1>
         <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <Link
-            href={process.env.NEXT_PUBLIC_APP_ONE_URL!}
-            passHref
-            legacyBehavior
-          >
+          <Link href={app_admin!} passHref legacyBehavior>
             <Button asChild variant="outline" className="m-2">
               <a>Go to Admin Portal</a>
             </Button>
           </Link>
-          <Link
-            href={process.env.NEXT_PUBLIC_APP_TWO_URL!}
-            passHref
-            legacyBehavior
-          >
+          <Link href={app_student!} passHref legacyBehavior>
             <Button asChild variant="outline" className="m-2">
               <a>Go to Student Portal</a>
             </Button>
